13469e177bce875dec147435b1b202b2b89492ac
supansawongkrod-create <supansa.wongkrod@gmail.com>
2026-09-19 20:37:36 +0800

    Update ordertracking.js

--- api/ordertracking.js
+++ api/ordertracking.js
@@ -34,7 +34,8 @@ export default async function handler(req, res) {
 
     if (data.result) {
       try {
-        trackingNumbers = JSON.parse(data.result);
+        const decoded = decodeURIComponent(data.result);
+        trackingNumber = JSON.parse(decoded);
       } catch {
         trackingNumbers = [];
       }
